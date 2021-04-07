8c2fc6129ad41ea42e4f8185da91e0d39129bb55
Kenedy Matiasso Portella <kenedyportella@hotmail.com>
2021-04-07 20:40:47 -0300

    create flight mechanics package

---
+++ pyaat/flightmechanics.py
@@ -0,0 +1,7 @@
+"""
+Python Aerospace Analysis Toolbox - PyAAT
+Copyright (c) 2021 Kenedy Matiasso Portella
+Distributed under MIT License
+
+This file contains methods and functions for performance analysis
+"""

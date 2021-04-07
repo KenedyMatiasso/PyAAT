f9b4035bc231ee2f87e1917015397b5f5ce1574c
Kenedy Matiasso Portella <kenedyportella@hotmail.com>
2021-04-07 20:34:29 -0300

    Implement root-finder for multiple roots

--- pyaat/tools.py
+++ pyaat/tools.py
@@ -8,11 +8,53 @@ This file contains most of tools that can be used by PyAAT.
 
 from numpy import cos, sin, array, transpose, tan, around
 from numpy import arctan2, arcsin, sqrt, degrees, radians, zeros, copy
-from numpy import trace, absolute
+from numpy import absolute, append, arange
 
 import matplotlib.pylab as plt
 import matplotlib
 matplotlib.rcParams['axes.formatter.useoffset'] = False
+
+from scipy.optimize import fsolve
+
+
+class RootFinder:
+    def __init__(self, start, stop, step=0.01, root_dtype="float64", xtol=1e-9):
+
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.xtol = xtol
+        self.roots = array([], dtype=root_dtype)
+
+    def add_to_roots(self, x):
+
+        if (x < self.start) or (x > self.stop):
+            return  # outside range
+        if any(abs(self.roots - x) < self.xtol):
+            return  # root already found.
+
+        self.roots = append(self.roots, x)
+
+    def find(self, f, *args):
+        current = self.start
+
+        for x0 in arange(self.start, self.stop + self.step, self.step):
+            if x0 < current:
+                continue
+            x = self.find_root(f, x0, *args)
+            if x is None:  # no root found.
+                continue
+            current = x
+            self.add_to_roots(x)
+
+        return self.roots
+
+    def find_root(self, f, x0, *args):
+
+        x, _, ier, _ = fsolve(f, x0=x0, args=args, full_output=True, xtol=self.xtol)
+        if ier == 1:
+            return x[0]
+        return None
 
 class plotter(object):
     def __init__(self):
